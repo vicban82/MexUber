7e9d1e630722500e40dd2e7c0775aa9c816647fa
Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas>
2023-09-19 13:59:48 -0600

    Cambios

--- src/components/Dashboard/styles.js
+++ src/components/Dashboard/styles.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 export const Container = styled.div`
@@ -23,4 +24,57 @@ export const Button = styled.button`
   border: none;
   padding: 10px;
   cursor: pointer;
+`;
+
+export const HamburgerButton = styled.div`
+  cursor: pointer;
+
+  .hamburger {
+    display: flex;
+    flex-direction: column;
+    width: 30px;
+    height: 24px;
+    margin: 6px 0;
+    background-color: #333; /* Color de las barras */
+    border-radius: 5px;
+    transition: 0.4s;
+
+    &.open {
+      background-color: transparent; /* Cambia a transparente cuando se abre */
+      .bar1 {
+        transform: rotate(-45deg) translate(-5px, 6px);
+      }
+      .bar2 {
+        opacity: 0;
+      }
+      .bar3 {
+        transform: rotate(45deg) translate(-5px, -6px);
+      }
+    }
+
+    .bar1,
+    .bar2,
+    .bar3 {
+      width: 100%;
+      height: 4px;
+      margin: 3px 0;
+      transition: 0.4s;
+    }
+  }
+`;
+export const NavButtonsContainer = styled.div`
+  margin-top: 20px;
+`;
+
+export const NavButton = styled(Link)`
+  display: block;
+  padding: 10px;
+  text-decoration: none;
+  color: #fff;
+  transition: background-color 0.3s ease;
+  margin-bottom: 5px; /* Margen inferior para separar los botones */
+
+  &:hover {
+    background-color: #555;
+  }
 `;
--- src/views/Dashboard/Main/index.jsx
+++ src/views/Dashboard/Main/index.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../../../components/Dashboard/styles";
+import { Link } from "react-router-dom";
 
 const MainDashboard = () => {
-  return <div>Main</div>;
+  const [selectedIcon, setSelectedIcon] = useState("main");
+  const handleIconClick = (icon) => {
+    setSelectedIcon(icon);
+  };
+  return (
+    <div>
+      <Button
+        onClick={() => handleIconClick("home")}
+        className={selectedIcon === "home" ? "active" : ""}
+      >
+        <Link to="/dashboard/home">Home</Link>
+      </Button>
+      <Button
+        onClick={() => handleIconClick("option1")}
+        className={selectedIcon === "option1" ? "active" : ""}
+      >
+        <Link to="/dashboard/option1">Option 1</Link>
+      </Button>
+      <Button
+        onClick={() => handleIconClick("option2")}
+        className={selectedIcon === "option2" ? "active" : ""}
+      >
+        <Link to="/dashboard/option2">Option 2</Link>
+      </Button>
+      <Button
+        onClick={() => handleIconClick("option3")}
+        className={selectedIcon === "option3" ? "active" : ""}
+      >
+        <Link to="/dashboard/option3">Option 3</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default MainDashboard;
