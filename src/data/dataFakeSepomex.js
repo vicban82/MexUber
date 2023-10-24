341452e0891b15b93e75d2505485195d40dc4e55
Cemb93 <karloz13m@gmail.com>
2023-10-24 13:40:34 -0500

    data fake

---
+++ src/data/dataFakeSepomex.js
@@ -0,0 +1,42 @@
+export const sepomex = [
+  {
+    _id: "652099777d5ae3c2d6c720ad",
+    codigoPostal: "20922",
+    estado: "AGUASCALIENTES",
+    ciudad: "Jesús María",
+    colonias: ["La Escalera", "El Calvario", "La Troje", "Bellavista"],
+  },
+  {
+    _id: "652099777d5ae3c2d6c720ae",
+    codigoPostal: "20920",
+    estado: "AGUASCALIENTES",
+    ciudad: "Jesús María",
+    colonias: ["Jesús María Centro"],
+  },
+  {
+    _id: "652099777d5ae3c2d6c720af",
+    codigoPostal: "20917",
+    estado: "AGUASCALIENTES",
+    ciudad: "Jesús María",
+    colonias: ["Pedernal Segundo"],
+  },
+  {
+    _id: "652099777d5ae3c2d6c720b0",
+    codigoPostal: "20916",
+    estado: "AGUASCALIENTES",
+    ciudad: "Jesús María",
+    colonias: [
+      "Cartuja Residencial",
+      "Paseos de las Haciendas",
+      "El Chichimeco",
+      "Parque Industrial Chichimeco",
+    ],
+  },
+  {
+    _id: "652099777d5ae3c2d6c720b1",
+    codigoPostal: "20914",
+    estado: "AGUASCALIENTES",
+    ciudad: "Jesús María",
+    colonias: ["La Granjita (Los Palillos)"],
+  },
+];
